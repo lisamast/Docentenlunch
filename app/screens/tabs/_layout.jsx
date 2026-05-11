46b0806097771e31fc0a2578802d8e9bf2ee804d
lisamast <9027431@student.zadkine.nl>
2026-05-11 12:39:57 +0200

    navbar

---
+++ app/screens/tabs/_layout.jsx
@@ -0,0 +1,58 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function TabsLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarLabel: () => null,
+                tabBarStyle: {
+                    height: 80,
+                    paddingBottom: 5,
+                    paddingTop: 10,
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: () => (
+                        <Ionicons name="home-outline" size={24} color="black" />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="add"
+                options={{
+                    title: 'Add',
+                    tabBarIcon: () => (
+                        <Ionicons name="add-outline" size={24} color="black" />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    tabBarIcon: () => (
+                        <Ionicons name="search-outline" size={24} color="black" />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    tabBarIcon: () => (
+                        <Ionicons name="person-outline" size={24} color="black" />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
+}
